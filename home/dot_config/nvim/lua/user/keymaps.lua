000842f4b4ba5f445d84431ac2a3201fbec2dadb
Zach Spoelstra <zspoelstra@gmail.com>
2022-07-20 20:52:48 -0400

    Leader key and telescope keymappings

--- home/dot_config/nvim/lua/user/keymaps.lua
+++ home/dot_config/nvim/lua/user/keymaps.lua
@@ -1,3 +1,6 @@
+-- Setup our leader key
+vim.g.mapleader = " "
+
 local keymap = vim.keymap
 
 -- Disable the arrow keys in normal mode
@@ -5,3 +8,7 @@ keymap.set('n', '<Left>', '')
 keymap.set('n', '<Right>', '')
 keymap.set('n', '<Up>', '')
 keymap.set('n', '<Down>', '')
+
+-- Telescope mappings
+keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>')
+keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>')
