5c3c117cfb4a24ee1e660c4d23df4c718fae05bc
Zach Spoelstra <zspoelstra@gmail.com>
2022-08-17 08:12:28 -0400

    Remove autocommand for packer

--- home/dot_config/nvim/lua/core/commands.lua
+++ home/dot_config/nvim/lua/core/commands.lua
@@ -3,11 +3,3 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*" },
   command = [[%s/\s\+$//e]],
 })
-
--- Autoreload neovim whenenver this file changes
-vim.cmd([[
-  augroup packer_user_config
-    autocmd!
-    autocmd BufWritePost plugins/init.lua source <afile> | PackerCompile
-  augroup end
-]])
