a1d3a06abbb3d334623ffe575be0e2147c67667f
Zach Spoelstra <zspoelstra@gmail.com>
2022-08-25 09:58:25 -0400

    Enable cursorline and relativenumber

--- home/dot_config/nvim/lua/core/options.lua
+++ home/dot_config/nvim/lua/core/options.lua
@@ -1,8 +1,10 @@
 local opt = vim.opt
 
 -- Basic settings
+opt.cursorline = true
 opt.expandtab = true
 opt.number = true
+opt.relativenumber = true
 opt.ruler = true
 opt.shiftwidth = 2
 opt.signcolumn = "number"
