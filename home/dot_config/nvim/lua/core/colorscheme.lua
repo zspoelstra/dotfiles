a0b675ffa3c87eb7f3950c99c867268fffbdb838
Zach Spoelstra <zspoelstra@gmail.com>
2022-09-23 09:11:16 -0400

    Fix colorscheme setup

--- home/dot_config/nvim/lua/core/colorscheme.lua
+++ home/dot_config/nvim/lua/core/colorscheme.lua
@@ -1,16 +1,3 @@
-local catppuccin_loaded, catppuccin = pcall(require, "catppuccin")
-if not catppuccin_loaded then
-  return
-end
-
-vim.g.catppuccin_flavour = "mocha"
-
-catppuccin.setup {
-  compile = {
-    enabled = true
-  }
-}
-
 local rose_pine_loaded, rose_pine = pcall(require, "rose-pine")
 if not rose_pine_loaded then
   return
