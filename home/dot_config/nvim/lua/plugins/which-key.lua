7db762b90969f2b62826069978566e5303259279
Zach Spoelstra <zspoelstra@gmail.com>
2022-09-28 07:48:57 -0400

    More mappings

--- home/dot_config/nvim/lua/plugins/which-key.lua
+++ home/dot_config/nvim/lua/plugins/which-key.lua
@@ -6,20 +6,24 @@ end
 wk.setup {}
 wk.register({
   ["<leader>"] = {
+    b = {
+      name = "+buffers",
+      b = { ":Telescope buffers<cr>", "Search Buffers" },
+      d = { ":bd", "Delete Buffer" },
+    },
     e = {
       name = "+explorer",
-      e = { "<cmd>Neotree toggle<cr>", "Toggle" },
-      f = { "<cmd>Neotree focus<cr>", "Focus" },
-      r = { "<cmd>Neotree reveal<cr>", "Reveal File" },
+      e = { ":Neotree toggle<cr>", "Toggle Explorer" },
+      f = { ":Neotree focus<cr>", "Focus Explorer" },
+      r = { ":Neotree reveal<cr>", "Reveal File" },
     },
     f = {
       name = "+file",
-      b = { "<cmd>Telescope buffers<cr>", "Find Buffers" },
-      f = { "<cmd>Telescope find_files<cr>", "Find File" },
-      g = { "<cmd>Telescope live_grep_args<cr>", "Live Grep" },
-      h = { "<cmd>Telescope help_tags<cr>", "Help Tags" },
-      o = { "<cmd>Telescope oldfiles<cr>", "Open Recent Files" },
-      r = { "<cmd>Telescope resume<cr>", "Resume" },
+      f = { ":Telescope find_files<cr>", "Find File" },
+      g = { ":Telescope live_grep_args<cr>", "Live Grep" },
+      h = { ":Telescope help_tags<cr>", "Help Tags" },
+      o = { ":Telescope oldfiles<cr>", "Open Recent Files" },
+      r = { ":Telescope resume<cr>", "Resume" },
     }
   }
 })
