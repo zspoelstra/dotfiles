54d50f94a2466ec3c87dae51f477cbcb420a4427
Zach Spoelstra <zspoelstra@gmail.com>
2022-09-29 13:40:20 -0400

    Remove autopairs & lualine configs

--- home/dot_config/nvim/lua/plugins/autopairs.lua
+++
@@ -1,6 +0,0 @@
-local loaded, autopairs = pcall(require, "nvim-autopairs")
-if not loaded then
-  return
-end
-
-autopairs.setup {}
--- home/dot_config/nvim/lua/plugins/lualine.lua
+++
@@ -1,6 +0,0 @@
-local lualine_ok, lualine = pcall(require, "lualine")
-if not lualine_ok then
-  return
-end
-
-lualine.setup()
