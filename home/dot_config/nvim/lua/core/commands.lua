06c5365b1bb058b18101dab3bc47dea11328c124
Zach Spoelstra <zspoelstra@gmail.com>
2023-01-14 17:44:34 -0500

    Fix for "No Folds Found" with treesitter folding

--- home/dot_config/nvim/lua/core/commands.lua
+++ home/dot_config/nvim/lua/core/commands.lua
@@ -3,3 +3,13 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*" },
   command = [[%s/\s\+$//e]],
 })
+
+-- Workaround for "No Folds Found"
+-- https://github.com/nvim-treesitter/nvim-treesitter/issues/1469
+vim.api.nvim_create_autocmd({'BufEnter','BufAdd','BufNew','BufNewFile','BufWinEnter'}, {
+  group = vim.api.nvim_create_augroup('TS_FOLD_WORKAROUND', {}),
+  callback = function()
+    vim.opt.foldmethod = 'expr'
+    vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+  end
+})
