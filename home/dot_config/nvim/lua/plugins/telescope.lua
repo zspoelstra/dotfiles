5ce871f57a53f40c23221d09be2133176fd1f91f
Zach Spoelstra <zspoelstra@gmail.com>
2023-01-24 14:29:32 -0500

    Live grep args mapping

--- home/dot_config/nvim/lua/plugins/telescope.lua
+++ home/dot_config/nvim/lua/plugins/telescope.lua
@@ -3,11 +3,26 @@ if not telescope_ok then
   return
 end
 
+local live_grep_args_ok, live_grep_args = pcall(require, "telescope-live-grep-args.actions")
+if not live_grep_args_ok then
+  return
+end
+
 telescope.setup {
   defaults = {
     file_ignore_patterns = { ".git/", "node_modules" },
     path_display = { "truncate" }
   },
+  extensions = {
+    live_grep_args = {
+      auto_quoting = true,
+      mappings = {
+        i = {
+          ["<C-k>"] = live_grep_args.quote_prompt(),
+        }
+      }
+    }
+  }
 }
 
 telescope.load_extension("fzf")
