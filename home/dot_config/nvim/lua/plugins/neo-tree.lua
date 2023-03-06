de587c8f95786b1e876590ebf1823043b3af321c
Zach Spoelstra <zspoelstra@gmail.com>
2023-03-06 14:14:22 -0500

    Adding neo-tree

---
+++ home/dot_config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,48 @@
+return {
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    keys = {
+      {
+        "<leader>fe",
+        function()
+          require("neo-tree.command").execute({ toggle = true })
+        end,
+        desc = "Explorer NeoTree",
+      },
+      { "<leader>e", "<leader>fe", desc = "Explorer", remap = true },
+    },
+    opts = {
+      close_if_last_window = true,
+      window = {
+        mappings = {
+          ["h"] = function(state)
+            local node = state.tree:get_node()
+            if node.type == "directory" and node:is_expanded() then
+              require("neo-tree.sources.filesystem").toggle_directory(state, node)
+            else
+              require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id())
+            end
+          end,
+          ["l"] = function(state)
+            local node = state.tree:get_node()
+            if node.type == "directory" then
+              if not node:is_expanded() then
+                require("neo-tree.sources.filesystem").toggle_directory(state, node)
+              elseif node:has_children() then
+                require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1])
+              end
+            end
+          end,
+        },
+      },
+    },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "kyazdani42/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    },
+    init = function()
+      vim.g.neo_tree_remove_legacy_commands = 1
+    end,
+  },
+}
