3052e3739ec1cf7c8c0e69758965c62c9e215901
Zach Spoelstra <zspoelstra@gmail.com>
2023-03-06 14:13:32 -0500

    Formatting

--- home/dot_config/nvim/lua/plugins/treesitter.lua
+++ home/dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,30 +1,30 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		version = false,
-		build = ":TSUpdate",
-		opts = {
-			highlight = { enable = true },
-			indent = { enable = true },
-			ensure_installed = {
-				"bash",
-				"help",
-				"html",
-				"javascript",
-				"json",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"regex",
-				"ruby",
-				"tsx",
-				"typescript",
-				"vim",
-				"yaml",
-			},
-		},
-		config = function(_, opts)
-			require("nvim-treesitter.configs").setup(opts)
-		end,
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    version = false,
+    build = ":TSUpdate",
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+      ensure_installed = {
+        "bash",
+        "help",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "regex",
+        "ruby",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+  },
 }
