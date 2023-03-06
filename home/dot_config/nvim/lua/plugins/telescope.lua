1c25aabd92a2931095e581db6718f739003673f8
Zach Spoelstra <zspoelstra@gmail.com>
2023-03-06 14:13:00 -0500

    Mostly formatting this

--- home/dot_config/nvim/lua/plugins/telescope.lua
+++ home/dot_config/nvim/lua/plugins/telescope.lua
@@ -1,29 +1,57 @@
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		version = "*",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			{
-				"nvim-telescope/telescope-fzf-native.nvim",
-				build = "make",
-				config = function()
-					require("telescope").load_extension("fzf")
-				end,
-			},
-		},
-		keys = {
-			{ "<leader>/", "<cmd>Telescope live_grep<cr>", desc = "Find in Files" },
-			{ "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-			{ "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
-			{ "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
-			{ "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
-		},
-		opts = {
-			defaults = {
-				prompt_prefix = " ",
-				selection_caret = " ",
-			},
-		},
-	},
+  {
+    "nvim-telescope/telescope.nvim",
+    version = "*",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        config = function()
+          require("telescope").load_extension("fzf")
+        end,
+      },
+    },
+    keys = {
+      {
+        "<leader>/",
+        function()
+          require("telescope.builtin").current_buffer_fuzzy_find(require("telescope.themes").get_dropdown({
+            winblend = 10,
+            previewer = false,
+          }))
+        end,
+      },
+
+      -- Find
+      { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+      { "<leader>fb", "<cmd>Telescope buffers<cr>", desc = "Buffers" },
+      { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
+
+      -- Search
+      { "<leader>sg", "<cmd>Telescope live_grep<cr>", desc = "Grep" },
+    },
+    opts = {
+      defaults = {
+        prompt_prefix = " ",
+        selection_caret = " ",
+        mappings = {
+          i = {
+            ["<C-j>"] = function(...)
+              return require("telescope.actions").move_selection_next(...)
+            end,
+            ["<C-k>"] = function(...)
+              return require("telescope.actions").move_selection_previous(...)
+            end,
+          },
+          n = {
+            ["q"] = function(...)
+              return require("telescope.actions").close(...)
+            end,
+          },
+        },
+      },
+    },
+  },
 }
