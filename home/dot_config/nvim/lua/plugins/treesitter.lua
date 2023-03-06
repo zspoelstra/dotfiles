bdf31b0227acdb620572b908ffc713a46720ba66
Zach Spoelstra <zspoelstra@gmail.com>
2023-03-06 16:46:11 -0500

    treesitter text objects

--- home/dot_config/nvim/lua/plugins/treesitter.lua
+++ home/dot_config/nvim/lua/plugins/treesitter.lua
@@ -3,6 +3,9 @@ return {
     "nvim-treesitter/nvim-treesitter",
     version = false,
     build = ":TSUpdate",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+    },
     opts = {
       highlight = { enable = true },
       indent = { enable = true },
@@ -21,6 +24,22 @@ return {
         "typescript",
         "vim",
         "yaml",
+      },
+      text_objects = {
+        select = {
+          enable = true,
+          lookahead = true,
+          keymaps = {
+            ["aa"] = { query = "@parameter.outer", desc = "Select outer parameter" },
+            ["ia"] = { query = "@parameter.inner", desc = "Select inner parameter" },
+            ["ab"] = { query = "@block.outer", desc = "Select outer block" },
+            ["ib"] = { query = "@block.inner", desc = "Select inner block" },
+            ["af"] = { query = "@function.outer", desc = "Select outer function" },
+            ["if"] = { query = "@function.inner", desc = "Select inner function" },
+            ["ac"] = { query = "@class.outer", desc = "Select outer class" },
+            ["ic"] = { query = "@class.inner", desc = "Select inner class" },
+          },
+        },
       },
     },
     config = function(_, opts)
