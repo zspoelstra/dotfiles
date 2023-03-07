a68b71096c1e0b8f10c521323f81c139f0caf99e
Zach Spoelstra <zspoelstra@gmail.com>
2023-03-06 21:17:55 -0500

    Move the main on_attach outside

--- home/dot_config/nvim/lua/plugins/nvim-lspconfig.lua
+++ home/dot_config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -21,7 +21,7 @@ return {
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
-      require("plugins.lsp.core").on_attach(function(client, bufnr)
+      require("util").on_attach(function(client, bufnr)
         require("plugins.lsp.format").on_attach(client, bufnr)
         require("plugins.lsp.keymaps").on_attach(client, bufnr)
       end)
