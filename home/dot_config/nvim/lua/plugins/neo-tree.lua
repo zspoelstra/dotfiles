076a4c37f2d6d24b52af464e8bfd3d16dcdf80fd
Zach Spoelstra <zspoelstra@gmail.com>
2023-03-08 12:19:36 -0500

    Make neotree work when opening with a directory

--- home/dot_config/nvim/lua/plugins/neo-tree.lua
+++ home/dot_config/nvim/lua/plugins/neo-tree.lua
@@ -44,6 +44,12 @@ return {
     },
     init = function()
       vim.g.neo_tree_remove_legacy_commands = 1
+      if vim.fn.argc() == 1 then
+        local stat = vim.loop.fs_stat(tostring(vim.fn.argv(0)))
+        if stat and stat.type == "directory" then
+          require("neo-tree")
+        end
+      end
     end,
   },
 }
