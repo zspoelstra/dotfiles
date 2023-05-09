6acc280a7edb92f1ec0bc911e1d79732547d31f3
Zach Spoelstra <zspoelstra@gmail.com>
2023-05-09 08:33:21 -0400

    Remove Kitty centering with hammerspoon

--- home/dot_config/hammerspoon/init.lua
+++ home/dot_config/hammerspoon/init.lua
@@ -1,14 +1,3 @@
 -- Automatic config reloading
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
-
--- Center the kitty window when launched
-function kittyCentering(appName, eventType, appObject)
-  if (eventType == hs.application.watcher.launched) then
-    if (appName == 'kitty') then
-      appObject:mainWindow():centerOnScreen()
-    end
-  end
-end
-
-hs.application.watcher.new(kittyCentering):start()
