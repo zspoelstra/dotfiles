7fd918b0067f5a76594704ee5c31d857adeb2530
Zach Spoelstra <zspoelstra@gmail.com>
2023-05-09 11:06:31 -0400

    Mostly stylua formatting. Command palette colors

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -1,36 +1,32 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
-local mux = wezterm.mux
 local act = wezterm.action
-
 local config = wezterm.config_builder()
 
-config.audible_bell = 'Disabled'
-config.color_scheme = 'Nord (base16)'
-config.font = wezterm.font_with_fallback {
-  { family = 'Fira Code Retina' },
-  'Menlo'
-}
+config.audible_bell = "Disabled"
+config.color_scheme = "Nord (base16)"
+config.command_palette_bg_color = "#3b4252"
+config.command_palette_fg_color = "#d8dee9"
+config.command_palette_font_size = 18.0
+config.font = wezterm.font_with_fallback({
+  { family = "Fira Code Retina" },
+  "Menlo",
+})
 config.font_size = 18.0
 config.hide_tab_bar_if_only_one_tab = true
 config.line_height = 1.2
 config.tab_bar_at_bottom = true
-config.window_decorations = 'RESIZE'
+config.window_decorations = "RESIZE"
 
--- config.disable_default_key_bindings = true
 config.keys = {
   {
-    key = 'k',
-    mods = 'CMD',
-    action = act.Multiple {
-      act.ClearScrollback 'ScrollbackAndViewport',
-      act.SendKey { key = 'L', mods = 'CTRL' },
-    },
+    key = "k",
+    mods = "CMD",
+    action = act.Multiple({
+      act.ClearScrollback("ScrollbackAndViewport"),
+      act.SendKey({ key = "L", mods = "CTRL" }),
+    }),
   },
 }
-
--- wezterm.on('gui-startup', function(window)
---   window:maximize()
--- end)
 
 return config
