bed9138376b810564267860969c05a0dac144b59
Zach Spoelstra <zspoelstra@gmail.com>
2023-05-10 09:11:06 -0400

    Remove wezterm window padding

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ local act = wezterm.action
 local config = wezterm.config_builder()
 
 config.audible_bell = "Disabled"
+config.check_for_updates = false
 config.color_scheme = "Nord (base16)"
 config.command_palette_bg_color = "#3b4252"
 config.command_palette_fg_color = "#d8dee9"
@@ -14,9 +15,16 @@ config.font = wezterm.font_with_fallback({
 })
 config.font_size = 18.0
 config.hide_tab_bar_if_only_one_tab = true
+config.initial_cols = 140
+config.initial_rows = 30
 config.line_height = 1.2
 config.tab_bar_at_bottom = true
-config.window_decorations = "RESIZE"
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
 
 config.keys = {
   {
