2c538a8eb8e16e7f7d8bd1e622fd7d06fd073348
Zach Spoelstra <zspoelstra@gmail.com>
2023-05-14 20:21:51 -0400

    Basic multiplexing setup

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -9,16 +9,21 @@ config.color_scheme = "Nord (base16)"
 config.command_palette_bg_color = "#3b4252"
 config.command_palette_fg_color = "#d8dee9"
 config.command_palette_font_size = 18.0
+config.default_gui_startup_args = { "connect", "unix" }
 config.font = wezterm.font_with_fallback({
   { family = "Fira Code Retina" },
   "Menlo",
 })
 config.font_size = 18.0
 config.hide_tab_bar_if_only_one_tab = true
-config.initial_cols = 140
-config.initial_rows = 30
+config.initial_cols = 120
+config.initial_rows = 28
 config.line_height = 1.2
 config.tab_bar_at_bottom = true
+config.unix_domains = {
+  { name = "unix" },
+}
+config.window_close_confirmation = "NeverPrompt"
 config.window_padding = {
   left = 0,
   right = 0,
