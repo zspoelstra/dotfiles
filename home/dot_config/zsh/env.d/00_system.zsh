3ae0e959fc28aae1eef5eb81e4ea616a1be5fcda
Zach Spoelstra <zspoelstra@gmail.com>
2024-05-20 17:22:38 -0400

    Fix pathing issue with asdf

--- home/dot_config/zsh/env.d/00_system.zsh
+++ home/dot_config/zsh/env.d/00_system.zsh
@@ -1,6 +1,7 @@
-# Disable Apple shell sessions
+# disable Apple shell sessions
 SHELL_SESSIONS_DISABLE=1
 
+# run the path_helper in the order that we want it
 # copied from /etc/zprofile
 if [ -x /usr/libexec/path_helper ]; then
   eval `/usr/libexec/path_helper -s`
--- home/dot_config/zsh/env.d/10_exports.zsh
+++ home/dot_config/zsh/env.d/10_exports.zsh
@@ -18,7 +18,6 @@ export XDG_CONFIG_HOME"=${HOME}/.config"
 export XDG_CACHE_HOME="${HOME}/.cache"
 export XDG_DATA_HOME="${HOME}/.local/share"
 export XDG_STATE_HOME="${HOME}/.local/state"
-# export XDG_RUNTIME_DIR="${TMPDIR:-/tmp}/runtime-${USER}"
 
 # XDG compliance (see xdg-ninga for help)
 # export ASDF_CONFIG_FILE="${XDG_CONFIG_HOME}/asdf/asdfrc"
