ae0a71d882ee450e02813a1c6b04979489d305a6
Zach Spoelstra <zspoelstra@gmail.com>
2024-05-21 14:46:02 -0400

    Fix SDKMan

--- home/dot_config/zsh/rc.d/90_sdkman.zsh
+++ home/dot_config/zsh/rc.d/90_sdkman.zsh
@@ -1,7 +1,3 @@
-() {
-  local sdkman_path="$HOME/.sdkman"
-  if [[ -d "$sdkman_path" ]] && [[ -s "${sdkman_path}/bin/sdkman-init.sh" ]]; then
-    export SDKMAN_DIR="${sdkman_path}"
-    source "${sdkman_path}/bin/sdkman-init.sh"
-  fi
-}
+if [[ -d "${SDKMAN_DIR}" ]] && [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]]; then
+  source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+fi
