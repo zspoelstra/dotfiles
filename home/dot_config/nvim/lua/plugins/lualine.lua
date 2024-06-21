58a6f1e50afa7d7a3b04cbe8dcc06dcc010f7610
Zach Spoelstra <zspoelstra@gmail.com>
2024-06-21 13:54:58 -0400

    Simplify lualine so we can build it

--- home/dot_config/nvim/lua/plugins/lualine.lua
+++ home/dot_config/nvim/lua/plugins/lualine.lua
@@ -5,6 +5,27 @@ local M = {
   end,
   dependencies = {
     { "nvim-tree/nvim-web-devicons" }
+  },
+  opts = {
+    options = {
+      component_separators = "",
+      section_separators = ""
+    },
+    sections = {
+      lualine_a = {
+        {
+          "mode",
+          fmt = function(str)
+            return str:sub(1,1)
+          end
+        }
+      },
+      lualine_b = { "filename" },
+      lualine_c = {},
+      lualine_x = {},
+      lualine_y = {},
+      lualine_z = {},
+    }
   }
 }
 
