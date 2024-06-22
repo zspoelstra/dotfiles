abf69499525d0f25f5139e44bb1ec3c775685507
Zach Spoelstra <zspoelstra@gmail.com>
2024-06-22 13:51:37 -0400

    Placeholder LspAttach auto command

--- home/dot_config/nvim/lua/plugins/lsp.lua
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -1,6 +1,13 @@
 local M = {
   "neovim/nvim-lspconfig",
   config = function(_, opts)
+    -- vim.api.nvim_create_autocmd("LspAttach", {
+    --   group = vim.api.nvim_create_augroup("zrs-lsp-attach"),
+    --   callback = function()
+    --     -- setup the keymaps here
+    --   end,
+    -- })
+
     require("mason").setup()
     require("mason-lspconfig").setup({
       ensure_installed = { "lua_ls" },
