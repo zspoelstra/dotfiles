1238eff7e77e7923d8cf57c18eb8b8e02d73b638
Zach Spoelstra <zspoelstra@gmail.com>
2024-06-24 08:51:42 -0400

    Basic wezterm config

---
+++ home/dot_config/wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.automatically_reload_config = true
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 18.0
+config.color_scheme = "Catppuccin Mocha"
+config.use_fancy_tab_bar = false
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+return config
