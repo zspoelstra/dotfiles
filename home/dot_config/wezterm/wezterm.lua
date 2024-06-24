cfc3d7c17cb0a437178f1c21a7ace110750be6c1
Zach Spoelstra <zspoelstra@gmail.com>
2024-06-24 16:55:33 -0400

    More wezterm setup

--- home/dot_config/wezterm/wezterm.lua
+++ home/dot_config/wezterm/wezterm.lua
@@ -1,11 +1,24 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 local config = {}
 
+config.audible_bell = "Disabled"
 config.automatically_reload_config = true
+config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("JetBrains Mono")
 config.font_size = 18.0
-config.color_scheme = "Catppuccin Mocha"
+config.keys = {
+  {
+    key = "k",
+    mods = "SUPER",
+    action = act.Multiple({
+      act.ClearScrollback("ScrollbackAndViewport"),
+      act.SendKey({ key = "L", mods = "CTRL" }),
+    }),
+  },
+}
 config.use_fancy_tab_bar = false
+config.window_close_confirmation = "NeverPrompt"
 config.window_decorations = "INTEGRATED_BUTTONS | RESIZE"
 config.window_padding = {
   left = 0,
