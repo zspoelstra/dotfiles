4c3d65565aa149a01b9e4969c273e02100adb51d
Zach Spoelstra <zspoelstra@gmail.com>
2024-12-17 08:51:12 -0500

    Telescope keymap for help tags

--- home/dot_config/nvim/lua/plugins/telescope.lua
+++ home/dot_config/nvim/lua/plugins/telescope.lua
@@ -2,16 +2,18 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     branch = "0.1.x",
-    config = function()
-      require("telescope").setup()
+    config = function(opts)
+      require("telescope").setup(opts)
       require("telescope").load_extension("fzf")
 
       local builtin = require("telescope.builtin")
       vim.keymap.set("n", "<leader>ff", builtin.find_files)
+      vim.keymap.set("n", "<leader>fh", builtin.help_tags)
     end,
     dependencies = {
       { "nvim-lua/plenary.nvim" },
       { "nvim-telescope/telescope-fzf-native.nvim", build = "make" }
     },
+    opts = {}
   }
 }
