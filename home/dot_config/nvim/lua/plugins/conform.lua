246a1e3ad6a1c02ca07569f3d4dfab7b6809fc6b
Zach Spoelstra <zspoelstra@gmail.com>
2024-12-27 12:01:26 -0500

    Prettier for JSON. Trim for all

--- home/dot_config/nvim/lua/plugins/conform.lua
+++ home/dot_config/nvim/lua/plugins/conform.lua
@@ -3,7 +3,9 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
+        json = { "prettierd", "prettier", stop_after_first = true },
         lua = { "stylua" },
+        ["*"] = { "trim_newlines", "trim_whitespace" },
       },
       format_on_save = {
         lsp_format = "fallback",
