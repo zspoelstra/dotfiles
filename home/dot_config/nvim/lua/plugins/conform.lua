63774f3249b545eb6b41aa3d301f6bf42af6d6cd
Zach Spoelstra <zspoelstra@gmail.com>
2025-04-22 09:28:50 -0400

    Remove commented lines from conform config

--- home/dot_config/nvim/lua/plugins/conform.lua
+++ home/dot_config/nvim/lua/plugins/conform.lua
@@ -3,8 +3,6 @@ return {
     "stevearc/conform.nvim",
     opts = {
       formatters_by_ft = {
-        -- json = { "prettierd", "prettier", stop_after_first = true },
-        -- lua = { "stylua" },
         ["*"] = { "trim_newlines", "trim_whitespace" },
       },
       format_on_save = {
