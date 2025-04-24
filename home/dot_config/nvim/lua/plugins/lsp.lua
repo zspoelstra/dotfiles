0c651a0f7a8a605a784a08805bcb07271be10daa
Zach Spoelstra <zspoelstra@gmail.com>
2025-04-24 15:41:24 -0400

    lua_ls lsp config

--- home/dot_config/nvim/lua/plugins/lsp.lua
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -2,7 +2,16 @@ return {
   {
     "neovim/nvim-lspconfig",
     config = function()
+      vim.lsp.config("lua_ls", {
+        settings = {
+          Lua = {
+            diagnostics = {
+              disable = { "missing-fields" },
+            },
+          },
+        },
+      })
       vim.lsp.enable("lua_ls")
-    end
-  }
+    end,
+  },
 }
