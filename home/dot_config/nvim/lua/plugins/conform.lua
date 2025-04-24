4f43e99ab7f67ebebc3c7111dfb463b8b46344aa
Zach Spoelstra <zspoelstra@gmail.com>
2025-04-24 15:39:46 -0400

    Enable using stylua for formatting

--- home/dot_config/nvim/lua/plugins/conform.lua
+++ home/dot_config/nvim/lua/plugins/conform.lua
@@ -4,6 +4,7 @@ return {
     opts = {
       formatters_by_ft = {
         ["*"] = { "trim_newlines", "trim_whitespace" },
+        lua = { "stylua" },
       },
       format_on_save = {
         lsp_format = "fallback",
