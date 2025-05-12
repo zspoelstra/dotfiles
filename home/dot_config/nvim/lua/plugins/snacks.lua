310d7904f6c40e66cf176830ac86ec0f51292937
Zach Spoelstra <zspoelstra@gmail.com>
2025-05-12 10:21:54 -0400

    Adding some Snacks.nvim keybinds

--- home/dot_config/nvim/lua/plugins/snacks.lua
+++ home/dot_config/nvim/lua/plugins/snacks.lua
@@ -2,17 +2,45 @@ return {
   "folke/snacks.nvim",
   keys = {
     {
+      "<leader>fb",
+      function ()
+        Snacks.picker.buffers()
+      end,
+      desc = "Find Buffer"
+    },
+    {
+      "<leader>fc",
+      function()
+        Snacks.picker.files({ cwd = vim.fn.stdpath("config") })
+      end,
+      desc = "Find Config File",
+    },
+    {
       "<leader>ff",
       function()
         Snacks.picker.files()
       end,
       desc = "Find files",
     },
+    {
+      "<leader>fp",
+      function()
+        Snacks.picker.projects()
+      end,
+      desc = "Find Projects",
+    },
   },
   lazy = false,
   opts = {
     indent = { enabled = true },
     picker = { enabled = true },
+    sources = {
+      picker = {
+        projects = {
+          dev = { "~/Code" },
+        },
+      },
+    },
     statuscolumn = { enabled = true },
   },
   priority = 1000,
