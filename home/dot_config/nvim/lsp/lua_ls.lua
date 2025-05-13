da16d6383552974b577da041f944697f4e220694
Zach Spoelstra <zspoelstra@gmail.com>
2025-05-13 09:27:30 -0400

    Move lua_ls config to lsp directory

---
+++ home/dot_config/nvim/lsp/lua_ls.lua
@@ -0,0 +1,9 @@
+return {
+  settings = {
+    Lua = {
+      diagnostics = {
+        disable = { "missing-fields" },
+      },
+    },
+  },
+}
--- home/dot_config/nvim/lua/plugins/lsp.lua
+++ home/dot_config/nvim/lua/plugins/lsp.lua
@@ -1,15 +1,6 @@
 return {
   "neovim/nvim-lspconfig",
   config = function()
-    vim.lsp.config("lua_ls", {
-      settings = {
-        Lua = {
-          diagnostics = {
-            disable = { "missing-fields" },
-          },
-        },
-      },
-    })
     vim.lsp.enable("lua_ls")
   end,
 }
