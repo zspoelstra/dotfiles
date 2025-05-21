0e092a072976a2736a8113bf6c1637453ddcdf0d
Zach Spoelstra <zspoelstra@gmail.com>
2025-05-20 22:07:13 -0400

    nvim: setting up more options

--- home/dot_config/nvim/lua/config/options.lua
+++ home/dot_config/nvim/lua/config/options.lua
@@ -10,10 +10,15 @@ vim.opt.clipboard = "unnamedplus"
 vim.opt.cursorline = true
 vim.opt.expandtab = true
 vim.opt.ignorecase = true
+vim.opt.list = true
+vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }
+vim.opt.mouse = "a"
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.scrolloff = 8
 vim.opt.shiftwidth = 2
 vim.opt.shortmess:append("I")
+vim.opt.showmode = false
 vim.opt.signcolumn = "yes"
 vim.opt.smartcase = true
 vim.opt.softtabstop = 0
@@ -24,3 +29,6 @@ vim.opt.termguicolors = true
 vim.opt.undofile = true
 vim.opt.winborder = "single"
 vim.opt.wrap = false
+
+-- Prepend mise shims to PATH
+-- vim.env.PATH = vim.env.HOME .. "/.local/share/mise/shims:" .. vim.env.PATH
