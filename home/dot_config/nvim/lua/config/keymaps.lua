8145766f30e2f09f645e35005f17e1eb04a49bbe
Zach Spoelstra <zspoelstra@gmail.com>
2025-05-20 22:08:27 -0400

    nvim: easier keymaps for moving window focus

--- home/dot_config/nvim/lua/config/keymaps.lua
+++ home/dot_config/nvim/lua/config/keymaps.lua
@@ -15,3 +15,9 @@ vim.keymap.set("n", "<leader>bd", function()
 end)
 vim.keymap.set("n", "<leader>bn", ":bnext<CR>")
 vim.keymap.set("n", "<leader>bp", ":bprevious<CR>")
+
+-- Easy window navigation
+vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Focus left window" })
+vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Focus lower window" })
+vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Focus upper window" })
+vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Focus right window" })
