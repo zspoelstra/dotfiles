2c6fa59e611cdc418a2838fb3b8ac8007ea2260e
Zach Spoelstra <zspoelstra@gmail.com>
2025-06-09 15:23:06 -0400

    nvim: add bash and html treesitter syntaxes

--- home/dot_config/nvim/lua/plugins/treesitter.lua
+++ home/dot_config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     require("nvim-treesitter.configs").setup({
       auto_install = false,
-      ensure_installed = { "lua", "markdown", "markdown_inline", "query", "regex", "ruby", "vim", "vimdoc" },
+      ensure_installed = { "bash", "html", "lua", "markdown", "markdown_inline", "query", "regex", "ruby", "vim", "vimdoc" },
       highlight = {
         additional_vim_regex_highlighting = false,
         enable = true,
