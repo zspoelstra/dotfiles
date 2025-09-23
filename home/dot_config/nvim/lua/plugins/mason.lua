331639f2e64abbd54519ead76d0955cd56b875d2
Zach Spoelstra <zspoelstra@gmail.com>
2025-09-23 11:59:07 -0400

    nvim: add mason.nvim to handle lsps, etc.

---
+++ home/dot_config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "mason-org/mason.nvim",
+    opts = {}
+  },
+  {
+    "mason-org/mason-lspconfig.nvim",
+    dependencies = {
+      "mason-org/mason.nvim",
+      "neovim/nvim-lspconfig"
+    },
+    opts = {
+      automatic_enable = false,
+      ensure_installed = {
+        "lua_ls"
+      }
+    }
+  }
+}
