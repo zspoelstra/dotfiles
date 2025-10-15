b13bbcc00ced08b2b4fd47d76d39fc887915ae28
Zach Spoelstra <zspoelstra@gmail.com>
2025-10-15 12:15:14 -0400

    nvim: prettier formatting for {java,type}script

--- home/dot_config/nvim/lua/plugins/conform.lua
+++ home/dot_config/nvim/lua/plugins/conform.lua
@@ -3,7 +3,11 @@ return {
   opts = {
     formatters_by_ft = {
       ["*"] = { "trim_newlines", "trim_whitespace" },
+      javascript = { "prettier" },
+      javascriptreact = { "prettier" },
       lua = { "stylua" },
+      typescript = { "prettier" },
+      typescriptreact = { "prettier" },
     },
     format_on_save = {
       lsp_format = "fallback",
