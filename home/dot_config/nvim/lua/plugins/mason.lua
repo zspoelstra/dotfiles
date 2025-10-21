4b12fd273e1c6dd42f99e294c8acfe4241c141bf
Zach Spoelstra <zspoelstra@gmail.com>
2025-10-21 16:29:27 -0400

    nvim: have mason install things automatically

--- home/dot_config/nvim/lua/plugins/mason.lua
+++ home/dot_config/nvim/lua/plugins/mason.lua
@@ -1,21 +1,26 @@
 return {
   {
     "mason-org/mason.nvim",
-    opts = {},
-  },
-  {
-    "mason-org/mason-lspconfig.nvim",
-    dependencies = {
-      "mason-org/mason.nvim",
-      "neovim/nvim-lspconfig",
-    },
     opts = {
-      automatic_enable = false,
       ensure_installed = {
-        "lua_ls",
+        "eslint_d",
+        "lua-language-server",
         "stylua",
         "vtsls",
       },
     },
+    config = function(_, opts)
+      require("mason").setup(opts)
+
+      local registry = require("mason-registry")
+      registry.refresh(function()
+        for _, tool in ipairs(opts.ensure_installed) do
+          local package = registry.get_package(tool)
+          if not package:is_installed() then
+            package:install()
+          end
+        end
+      end)
+    end,
   },
 }
