37691ff48d3a506bea2e64c3488ab6f0188c8c2b
Zach Spoelstra <zspoelstra@gmail.com>
2025-10-21 16:29:04 -0400

    nvim: add gre mapping to show line diagnostics (temporary)

--- home/dot_config/nvim/lua/config/autocmds.lua
+++ home/dot_config/nvim/lua/config/autocmds.lua
@@ -17,11 +17,13 @@ vim.api.nvim_create_autocmd("LspAttach", {
   group = augroup("lsp"),
   callback = function(args)
     local bufnr = args.buf
-    local client = vim.lsp.get_client_by_id(args.data.client_id)
 
-    if client and client:supports_method("textDocument/inlayHint") then
-      vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
-    end
+    vim.keymap.set(
+      "n",
+      "gre",
+      vim.diagnostic.open_float,
+      { buffer = bufnr, desc = "Show Line Diagnostic", silent = true }
+    )
 
     vim.keymap.set("n", "gd", function()
       Snacks.picker.lsp_definitions()
