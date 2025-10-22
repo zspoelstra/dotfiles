3d712ea6ede5bba64ce092282d7a7af7e68659a4
Zach Spoelstra <zspoelstra@gmail.com>
2025-10-22 18:52:30 -0400

    nvim: a whole lot more treesitter grammars

--- home/dot_config/nvim/lua/plugins/treesitter.lua
+++ home/dot_config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,25 @@ return {
   config = function()
     require("nvim-treesitter.configs").setup({
       auto_install = false,
-      ensure_installed = { "bash", "html", "lua", "markdown", "markdown_inline", "query", "regex", "ruby", "vim", "vimdoc" },
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "jsonc",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "query",
+        "regex",
+        "ruby",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
       highlight = {
         additional_vim_regex_highlighting = false,
         enable = true,
