be5f3f858e93cc90b112f9ce73d9a97de7e0079f
Zach Spoelstra <zspoelstra@gmail.com>
2025-10-24 14:39:02 -0400

    nvim: make h only fold on the first column

--- home/dot_config/nvim/lua/plugins/origami.lua
+++ home/dot_config/nvim/lua/plugins/origami.lua
@@ -1,7 +1,11 @@
 return {
   "chrisgrieser/nvim-origami",
   event = "VeryLazy",
-  opts = {},
+  opts = {
+    foldKeymaps = {
+      hOnlyOpensOnFirstColumn = true,
+    },
+  },
   init = function()
     vim.opt.foldlevel = 99
     vim.opt.foldlevelstart = 99
