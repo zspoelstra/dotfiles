58990bd0d1fa1972dd5c1b950b02a1b8eecd15fd
Zach Spoelstra <zspoelstra@gmail.com>
2026-01-23 15:51:18 -0500

    fish: remove configs

--- home/dot_config/fish/functions/aws-profile-set.fish
+++
@@ -1,6 +0,0 @@
-function aws-profile-set
-  set -gx AWS_PROFILE $argv[1]
-  aws iam list-account-aliases &> /dev/null || aws sso login
-end
-
-complete -c aws-profile-set -f -a "(aws configure list-profiles | string split '\n')"
--- home/dot_config/fish/functions/aws-region-set.fish
+++
@@ -1,6 +0,0 @@
-function aws-region-set
-  set -gx AWS_REGION $argv[1]
-  echo "AWS_REGION set to $AWS_REGION"
-end
-
-complete -c aws-region-set -f -a "us-east-1 us-east-2"
