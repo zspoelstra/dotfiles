205d7893dbff9d410255b906cbe9382de2f6d9db
Zach Spoelstra <zspoelstra@gmail.com>
2026-01-27 08:10:28 -0500

    nvim: add render-markdown plugin

---
+++ home/dot_config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,8 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = {
+    { "nvim-mini/mini.icons", opts = {} },
+    { "nvim-treesitter/nvim-treesitter" },
+  },
+  opts = {},
+}
